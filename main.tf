c330a0dd24bb12c4cbca4211feb2a33fb873ff6a
BCIs-maniac81 <bendibhafededdine@gmail.com>
2023-08-29 13:28:54 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,82 @@
+# IAM Role
+resource "aws_iam_role" "lambda_role" {
+  name               = "terraform_aws_lambda_role"
+  assume_role_policy = <<EOF
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Action": "sts:AssumeRole",
+                "Principal": {
+                    "Service": "lambda.amazonaws.com"
+                },
+            "Effect": "Allow",
+            "Sid": ""
+            }
+        ]
+    }
+    EOF
+}
+
+# IAM Policy for logging from lambda
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*",
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+# Policy Attachment on the role
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+}
+
+# Generates an archive from content
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/python_code/"
+  output_path = "${path.module}/python_code/python_code.zip"
+}
+# in terraform ${path.module} is the current directory
+
+# Create a Lambda function
+
+resource "aws_lambda_function" "lambda_function" {
+  filename      = "${path.module}/python_code/python_code.zip"
+  function_name = "hello-lambda-function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "python_code.lambda_handler"
+  runtime       = "python3.11"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
+output "terraform_aws_role_output" {
+  value = aws_iam_role.lambda_role.name
+}
+
+output "terraform_aws_role_arn_output" {
+  value = aws_iam_role.lambda_role.arn
+}
+
+output "terraform_logging_arn_output" {
+  value = aws_iam_policy.iam_policy_for_lambda.arn
+}
